9b9350dd0589c64ff079dce7bf20b709c6072323
63070150 <63070150@it.kmitl.ac.th>
2023-02-27 20:58:11 +0700

    please just commit dafaq

---
+++ data.js
@@ -0,0 +1,26 @@
+// alt+96
+const books = JSON.parse(`[{
+    "name": "Eren",
+    "author": "Hajime Isayama",
+    "src": "assets/eren.png"
+}, {
+    "name": "Armin",
+    "author": "Hajime Isayama",
+    "src": "assets/armin.jfif"
+}, {
+    "name": "Akame",
+    "author": "Takahiro",
+    "src": "assets/akame.jpg"
+}, {
+    "name": "Eren",
+    "author": "Hajime Isayama",
+    "src": "assets/eren.png"
+}, {
+    "name": "Armin",
+    "author": "Hajime Isayama",
+    "src": "assets/armin.jfif"
+}, {
+    "name": "Akame",
+    "author": "Takahiro",
+    "src": "assets/akame.jpg"
+}]`)
