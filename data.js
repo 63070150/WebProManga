e91968e725bb9a2fbcb5436897e88703f70d6c57
63070150 <63070150@it.kmitl.ac.th>
2023-03-01 21:36:32 +0700

    commit finally??

--- data.js
+++ data.js
@@ -1,26 +1,38 @@
 // alt+96
 const books = JSON.parse(`[{
-    "name": "Eren",
+    "name": "Eren1",
     "author": "Hajime Isayama",
-    "src": "assets/eren.png"
+    "src": "assets/eren.png",
+    "isFavorite": false,
+    "book_id": 123
 }, {
-    "name": "Armin",
+    "name": "Armin2",
     "author": "Hajime Isayama",
-    "src": "assets/armin.jfif"
+    "src": "assets/armin.jfif",
+    "isFavorite": false,
+    "book_id": 124
 }, {
-    "name": "Akame",
+    "name": "Akame3",
     "author": "Takahiro",
-    "src": "assets/akame.jpg"
+    "src": "assets/akame.jpg",
+    "isFavorite": false,
+    "book_id": 125
 }, {
-    "name": "Eren",
+    "name": "Eren4",
     "author": "Hajime Isayama",
-    "src": "assets/eren.png"
+    "src": "assets/eren.png",
+    "isFavorite": false,
+    "book_id": 126
 }, {
-    "name": "Armin",
+    "name": "Armin5",
     "author": "Hajime Isayama",
-    "src": "assets/armin.jfif"
+    "src": "assets/armin.jfif",
+    "isFavorite": false,
+    "book_id": 127
 }, {
-    "name": "Akame",
+    "name": "Akame6",
     "author": "Takahiro",
-    "src": "assets/akame.jpg"
+    "src": "assets/akame.jpg",
+    "isFavorite": false,
+    "book_id": 128
 }]`)
