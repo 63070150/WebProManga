5e7b53ae4d623c106f93008babece2ce1222a744
63070150 <63070150@it.kmitl.ac.th>
2023-03-02 01:07:50 +0700

    localtest vue still shit

--- vue.js
+++ vue.js
@@ -13,12 +13,23 @@ var app = new Vue({
     methods: {
       addfav(index){
         let book = this.books[index]//corrected
-        if(this.favlist.includes(book)){//??gotta find which book_id in favlist that equal to it then splice it  but idk how to get index from favlist
-          let fbookindex = this.favlist.map(item => item.book_id).indexOf(book.book_id)
-          this.favlist.splice(fbookindex, 1)
+        if(this.getfav().includes(book)){//??gotta find which book_id in favlist that equal to it then splice it  but idk how to get index from favlist
+          alert('getfav includes book')
+          let fbookindex = this.getfav().map(item => item.book_id).indexOf(book.book_id)
+          this.getfav().splice(fbookindex, 1)
+          localStorage.clear()
+          localStorage.setItem("favlist", JSON.stringify(this.favlist))
         }else{
-          this.favlist.push(books[index])
+          alert('nope')
+          this.favlist.push(book)// need
+          this.setfav(this.favlist)
         }
+      },
+      getfav(){
+        return JSON.parse(localStorage.getItem('favlist'))//got favlist[obj, obj, obj,...]
+      },
+      setfav(obj){
+        localStorage.setItem("favlist", JSON.stringify(obj))//send to storage got 'favlist[obj, obj, obj,]'
       }
     }
 })
