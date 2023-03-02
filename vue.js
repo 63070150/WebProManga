854479541ed3ca71327787c16f22105e3d22fcf9
63070150 <63070150@it.kmitl.ac.th>
2023-03-02 21:11:29 +0700

    how to use vue in favorite.html

--- vue.js
+++ vue.js
@@ -9,27 +9,26 @@ var app = new Vue({
         score: '',
         books: books,
         favlist: [],
+        getfavlist: [],
+        notnull: false,
     },
     methods: {
       addfav(index){
-        let book = this.books[index]//corrected
-        let getfav = JSON.parse(localStorage.getItem('favlist'))
-        //if(getfav.includes(book)){
-          //??gotta find which book_id in favlist that equal to it then splice it  but idk how to get index from favlist
-        if(false){
-          let fbookindex = getfav.map(item => item.book_id).indexOf(book.book_id)
-          getfav.splice(fbookindex, 1)
+        let book = this.books[index]// get that book   corrected
+        if(this.favlist.includes(book)){
+          let getfav = JSON.parse(localStorage.getItem('favlist'))//get favorite list
+          let fbookindex = getfav.map(item => item.book_id).indexOf(book.book_id)//check list in localstorage
+          this.favlist.splice(fbookindex, 1)//splice both favlist and getfav
           localStorage.clear()
-          localStorage.setItem("favlist", JSON.stringify(getfav))
-        }else{
-          this.favlist.push(book)// need   correct
-          localStorage.setItem("favlist", JSON.stringify(this.favlist))//correct
-          console.log(getfav);
+          localStorage.setItem("favlist", JSON.stringify(this.favlist))
+        }
+        else{
+          this.favlist.push(book);localStorage.setItem("favlist", JSON.stringify(this.favlist))//correct
         }
       },
-      // getfav(){
-      //   return JSON.parse(localStorage.getItem('favlist'));//got favlist[obj, obj, obj,...]
-      // },
+      funcgetfav(){
+        return JSON.parse(localStorage.getItem('favlist'));//got favlist[obj, obj, obj,...]
+      },
       // setfav(obj){
       //   localStorage.setItem("favlist", JSON.stringify(obj));//send to storage got 'favlist[obj, obj, obj,]'
       // }
