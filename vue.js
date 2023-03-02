33848ac3c9bf1e91c6aaa777075a5e92bf3f9c22
63070150 <63070150@it.kmitl.ac.th>
2023-03-02 10:39:48 +0700

    localtest error at vue

--- vue.js
+++ vue.js
@@ -13,23 +13,23 @@ var app = new Vue({
     methods: {
       addfav(index){
         let book = this.books[index]//corrected
-        if(this.getfav().includes(book)){//??gotta find which book_id in favlist that equal to it then splice it  but idk how to get index from favlist
-          alert('getfav includes book')
-          let fbookindex = this.getfav().map(item => item.book_id).indexOf(book.book_id)
-          this.getfav().splice(fbookindex, 1)
+        if(this.getfav().includes(book)){                                 //??gotta find which book_id in favlist that equal to it then splice it  but idk how to get index from favlist
+          let fav = this.getfav()
+          console.log(fav);
+          let fbookindex = fav.map(item => item.book_id).indexOf(book.book_id)
+          fav.splice(fbookindex, 1)
           localStorage.clear()
-          localStorage.setItem("favlist", JSON.stringify(this.favlist))
+          localStorage.setItem("favlist", JSON.stringify(fav))
         }else{
-          alert('nope')
           this.favlist.push(book)// need
           this.setfav(this.favlist)
         }
       },
       getfav(){
-        return JSON.parse(localStorage.getItem('favlist'))//got favlist[obj, obj, obj,...]
+        return JSON.parse(localStorage.getItem('favlist'));//got favlist[obj, obj, obj,...]
       },
       setfav(obj){
-        localStorage.setItem("favlist", JSON.stringify(obj))//send to storage got 'favlist[obj, obj, obj,]'
+        localStorage.setItem("favlist", JSON.stringify(obj));//send to storage got 'favlist[obj, obj, obj,]'
       }
     }
 })
